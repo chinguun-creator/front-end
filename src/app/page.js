62c11ad82d8bd15938e55447e4a428bc52946268
PomeDe <pomedev21@gmail.com>
2026-03-11 14:13:56 +0800

    prjoect ehellee

--- src/app/page.js
+++ src/app/page.js
@@ -2,102 +2,104 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex gap-4 m-4">
+      <div className="w-[6%] h-dvh bg-[#202B3B] rounded-[2vw]"></div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="w-[54%] h-dvh ">
+        <div className="w-full h-[8%] flex items-center justify-center bg-[#202B3B] rounded-[1vw]">
+          <input
+            type="text"
+            placeholder="Search for cities"
+            className="text-white px-4 w-full h-full focus:outline-none"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="flex items-center ml-20 mt-4">
+          <div>
+            <div className="text-white font-bold text-5xl">Madrid</div>
+            <div className="text-gray-400 text-sm mt-1">Chance of rain: 0%</div>
+            <div className="text-white font-bold text-6xl mt-4">31°</div>
+          </div>
+          <img
+            src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+            alt="Sunny"
+            className="w-28 h-28 right-4"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="w-full h-[30%] flex flex-col p-6 mt-4 bg-[#202B3B] rounded-[2vw]">
+          <p className="text-[#8499A2] font-bold text-xl place-self-start mb-4">
+            TODAY'S FORECAST
+          </p>
+          <div className="ml-6 flex items-center gap-4 text-[#8499A2]">
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+
+            <div className="border-r h-full pr-4 flex flex-col items-center gap-2">
+              <p>6:00 AM</p>
+              <img
+                src="https://cdn-icons-png.freepik.com/512/9744/9744519.png"
+                alt="Sunny"
+                className="w-24 h-24"
+              />
+              <span>20°</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[30%] flex items-center mt-4 bg-[#202B3B] rounded-[2vw]">
+          <div className="text-[#8499A2] font-bold text-xl ml-6 place-self-start mt-6">
+            AIR CONDITIONS
+          </div>
+        </div>
+      </div>
+
+      <div className="w-[40%] h-[calc(100vh-120px)] bg-[#202B3B] rounded-[2vw] mt-20">
+        <div className="text-[#8499A2] font-bold text-xl ml-4 place-self-start mt-6">
+          7 DAY FORECAST
+        </div>
+      </div>
     </div>
   );
 }
